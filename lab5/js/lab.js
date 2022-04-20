dc6b2b3a5b3f25845d6a9a80a09dc53401a74e12
LJ McDermott <Ljventuraca@gmail.com>
2022-04-20 11:09:44 -0700

    yay finished lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+// Summary: This is my first attempt doing this.
+// Description: Learning how to code within Javascript.
+// by: Lindsey McDermott.
+// date: 4-20-2022.
+
+
+// Define Variables
+make = "Toyota";
+model = "Tacoma";
+color = "Blue";
+year = 1995;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: "+ make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
